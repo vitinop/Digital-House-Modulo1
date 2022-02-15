822306437d222cb82cddb0411130799de698bd60
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-02-15 15:33:12 -0300

    Update mesaDeTrabalhoaula.js

--- Programação-Imperativa/mesaDeTrabalhoaula.js
+++ Programação-Imperativa/mesaDeTrabalhoaula.js
@@ -1,7 +1,7 @@
-const nome1= 'Aline Ferreira';
-const nome2= 'Ana Paula';
-const nome3= 'Carlos de Souza';
-const nome4= 'José da Silva';
+const nome1 = "Aline Ferreira";
+const nome2 = "Ana Paula";
+const nome3 = "Carlos de Souza";
+const nome4 = "José da Silva";
 
 var idade1 = 33;
 var idade2 = 26;
@@ -9,26 +9,62 @@ var idade3 = 28;
 var idade4 = 27;
 
 var peso1 = 63.7;
-var peso2 = 55.0 ;
-var peso3 = 80.1 ;
+var peso2 = 55.0;
+var peso3 = 80.1;
 var peso4 = 83.5;
 
-var altura1 =  1.53;
-var altura2= 1.62;
-var altura3=1.76;
-var altura4=1.70;
+var altura1 = 1.53;
+var altura2 = 1.62;
+var altura3 = 1.76;
+var altura4 = 1.7;
 
-var plano1 = true; 
-var plano2 = false; 
-var plano3 = true; 
-var plano4= true;
+var plano1 = true;
+var plano2 = false;
+var plano3 = true;
+var plano4 = true;
 
-imc1= peso1/altura1**2
-imc2= peso2/altura2**2
-imc3= peso3/altura3**2
-imc4= peso4/altura4**2
+imc1 = peso1 / altura1 ** 2;
+imc2 = peso2 / altura2 ** 2;
+imc3 = peso3 / altura3 ** 2;
+imc4 = peso4 / altura4 ** 2;
 
-console.log("O paciente" + nome1 +" com peso de" + peso1+" e altura de "+ altura1+", possui IMC equivalente a : "+ imc1)
-console.log("O paciente" + nome2 +" com peso de" + peso2+" e altura de "+ altura2+", possui IMC equivalente a : "+ imc2)
-console.log("O paciente" + nome3 +" com peso de" + peso3+" e altura de "+ altura3+", possui IMC equivalente a : "+ imc3)
-console.log("O paciente" + nome4 +" com peso de" + peso4+" e altura de "+ altura4+", possui IMC equivalente a : "+ imc4)
+console.log(
+  "O paciente" +
+    nome1 +
+    " com peso de" +
+    peso1 +
+    " e altura de " +
+    altura1 +
+    ", possui IMC equivalente a : " +
+    imc1
+);
+console.log(
+  "O paciente" +
+    nome2 +
+    " com peso de" +
+    peso2 +
+    " e altura de " +
+    altura2 +
+    ", possui IMC equivalente a : " +
+    imc2
+);
+console.log(
+  "O paciente" +
+    nome3 +
+    " com peso de" +
+    peso3 +
+    " e altura de " +
+    altura3 +
+    ", possui IMC equivalente a : " +
+    imc3
+);
+console.log(
+  "O paciente" +
+    nome4 +
+    " com peso de" +
+    peso4 +
+    " e altura de " +
+    altura4 +
+    ", possui IMC equivalente a : " +
+    imc4
+);
